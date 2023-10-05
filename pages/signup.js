20eb147dfd2abc479d02cf05eb27d6982a173e53
jongju-Lee <jeje7@naver.com>
2023-10-06 08:22:13 +0900

    good morning

--- pages/signup.js
+++ pages/signup.js
@@ -1,23 +1,37 @@
 import React, { useCallback, useState } from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
-import { Form, Input } from "antd";
+import { Form, Input, Checkbox, Button } from "antd";
+
+// 회원가입 페이지 만들기(커스텀 훅) 10 : 10
+// 2023. 10. 06 오전 01 : 22
 
 const Signup = () => {
   const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+
   const onChangeId = useCallback((e) => {
     setId(e.target.value);
   }, []);
 
-  const [password, setPassword] = useState("");
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
   }, []);
 
-  const [nickname, setNickname] = useState("");
   const onChangeNickName = useCallback((e) => {
     setNickname(e.target.value);
   }, []);
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
 
   const onSubmit = useCallback(() => {
     // onFinish 함수라서 e.preventDefault 자동적용됨.
@@ -53,6 +67,17 @@ const Signup = () => {
             value={password}
             required
             onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호 체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
           />
         </div>
       </Form>
